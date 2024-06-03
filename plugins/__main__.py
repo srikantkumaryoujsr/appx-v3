0824ba9a83ffbee2ff5161365fdb50176cc59497
vspji <155125763+vspji@users.noreply.github.com>
2024-06-03 20:09:23 +0530

    Add files via upload

---
+++ plugins/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from plugins import LOGGER, bot as app
+from plugins.modules import ALL_MODULES
+
+async def _start():
+    try:
+        await app.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+    for all_module in ALL_MODULES:
+        importlib.import_module("plugins.modules." + all_module)
+
+    LOGGER.info(f"@{app.username} Started.")
+    await app.send_message(6741261680, "I am Alive")
+    await idle()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(_start())
+    finally:
+        loop.close()
+    LOGGER.info("Stopping bot")
