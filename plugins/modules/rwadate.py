01af72a0bfc059232efb27c0266756b5f07069bc
vspji <155125763+vspji@users.noreply.github.com>
2024-06-05 11:54:56 +0530

    Update rwadate.py

--- plugins/modules/rwadate.py
+++ plugins/modules/rwadate.py
@@ -145,8 +145,8 @@ async def account_logins(bot,subjectid,chatid):
 scheduler.add_job(
     func=all_subject_send,
      trigger="cron",
-     hour=11,
-     minute=16,
+     hour=12,
+     minute=0,
      second=0, 
      args=[Client]
 )
