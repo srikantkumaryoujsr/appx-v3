b0786a67e47f39b8a23d491ded75bbc042fc0d44
vspji <155125763+vspji@users.noreply.github.com>
2024-09-07 09:51:54 +0530

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -15,7 +15,7 @@ async def _start():
         importlib.import_module("plugins.modules." + all_module)
 
     LOGGER.info(f"@{app.username} Started.")
-    await app.send_message(6748451207, "I am Alive")
+    await app.send_message(7513565186, "I am Alive")
     await idle()
 
 if __name__ == "__main__":
