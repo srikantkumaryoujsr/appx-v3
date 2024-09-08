5e72fc010c7e3f0d59ab5d6771eeddf638825a97
vspji <155125763+vspji@users.noreply.github.com>
2024-09-08 12:33:39 +0530

    Update sscgdrwa.py

--- plugins/modules/sscgdrwa.py
+++ plugins/modules/sscgdrwa.py
@@ -186,8 +186,8 @@ async def account_logins(bot, subjectid, chatid):
 scheduler.add_job(
     func=all_subject_send,
     trigger="cron",
-    hour=11,
-    minute=23,
+    hour=12,
+    minute=36,
     second=0, 
     args=[Client]
 )
