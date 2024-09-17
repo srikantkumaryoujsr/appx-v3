1e341639b9f39728f34a0e2933270ec654dd4590
vspji <155125763+vspji@users.noreply.github.com>
2024-09-17 07:50:08 +0530

    Update utils.py

--- plugins/modules/utils.py
+++ plugins/modules/utils.py
@@ -90,7 +90,7 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "⚫" * completed_length + "⚪" * remaining_length
+            progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
                 await reply.edit(f'\n ╭──⌯📤𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠📤⌯──╮ \n├✦ {progress_bar}|﹝{perc}﹞ \n├✦ 𝐬𝐩𝐞𝐞𝐝 ➠ {sp} \n├✦ 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐞𝐝 ➠ {cur}\n├✦ 𝐒𝐢𝐳𝐞 ➛ 𝐄𝐓𝐀 ➠ {tot} - {eta} \n╰─═❤️@ImTgHacker❤️═─╯\n') 
