6cee5c4e8abee2167efe68baf48b3b8058ea8afe
vspji <155125763+vspji@users.noreply.github.com>
2024-09-19 07:11:46 +0530

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -1,12 +1,9 @@
-# @sarkari_student
+#@sarkari_student
 import asyncio
 import importlib
-from pyrogram import idle, Client
+from pyrogram import idle
 from plugins import LOGGER, bot as app
 from plugins.modules import ALL_MODULES
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
 
 async def _start():
     try:
@@ -14,16 +11,11 @@ async def _start():
     except Exception as ex:
         LOGGER.error(ex)
         quit(1)
-    
-    # Initialize the scheduler with default time
-    scheduler.add_job(func=all_subject_send, trigger="cron", hour=6, minute=1, second=0, args=[app])
-    
     for all_module in ALL_MODULES:
         importlib.import_module("plugins.modules." + all_module)
 
     LOGGER.info(f"@{app.username} Started.")
-    await app.send_message(7513565186, "I am Alive")
-    scheduler.start()  # Start the scheduler
+    await app.send_message(6748451207, "I am Alive")
     await idle()
 
 if __name__ == "__main__":
