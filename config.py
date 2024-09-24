5d76f85ace36206d150d8c6623cabc033fa8479b
vspji <155125763+vspji@users.noreply.github.com>
2024-09-24 19:59:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7264837459:AAHjBYA57HGMvh_7uE0Jv1husTxNkkrf8Hs")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7584040377:AAGiQBvX1nh-9NG64HXPvwIjmNpXoxz2toE")
     API_ID = int(os.environ.get("API_ID", "20863912"))
     API_HASH = os.environ.get("API_HASH", "a10be717d1bc18303a48c6a7b0623849")
     AUTH_USERS = "7513565186"
