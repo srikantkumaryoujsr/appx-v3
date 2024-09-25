be67e291001b9d485f63c219a6797bf8536c8411
vspji <155125763+vspji@users.noreply.github.com>
2024-09-25 12:29:00 +0530

    Update utils.py

--- plugins/modules/utils.py
+++ plugins/modules/utils.py
@@ -93,6 +93,14 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
-                await reply.edit(f'\n ╭──⌯📤𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠📤⌯──╮ \n├✦ {progress_bar}|﹝{perc}﹞ \n├✦ 𝐬𝐩𝐞𝐞𝐝 ➠ {sp} \n├✦ 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐞𝐝 ➠ {cur}\n├✦ 𝐒𝐢𝐳𝐞 ➛ 𝐄𝐓𝐀 ➠ {tot} - {eta} \n╰─═❤️ᴍʀ_ʜᴀᴄᴋᴇʀ❤️═─╯\n') 
-            except FloodWait as e:
-                time.sleep(e.x)
+    await reply.edit(
+        f'\n ╭──⌯📤𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠📤⌯──╮ \n'
+        f'├✦ {progress_bar}|﹝{perc}﹞ \n'
+        f'├✦ 𝐬𝐩𝐞𝐞𝐝 ➠ {sp} \n'
+        f'├✦ 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐞𝐝 ➠ {cur}\n'
+        f'├✦ 𝐒𝐢𝐳𝐞 ➛ 𝐄𝐓𝐀 ➠ {tot} - {eta} \n'
+        f'╰─═❤️ᴍʀ_ʜᴀᴄᴋᴇʀ❤️═─╯\n',
+        reply_to_message_id=message_thread_id
+    )
+except FloodWait as e:
+    time.sleep(e.x)
