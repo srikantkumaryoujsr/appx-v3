4d693cb75b0d2e65225e3eae58fdc88da5c30767
vspteam <vidyannajaysinghpatel@gmail.com>
2024-11-08 08:16:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7259728131:AAHHrbBzhNZphPmiQrSMb49346lDZYctkU0")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7508390450:AAEp39kEUvmtwnmHhgZ2EO6QscIhG9hrRPE")
     API_ID = int(os.environ.get("API_ID", "20821267"))
     API_HASH = os.environ.get("API_HASH", "8723cdf433be176300044547ee6bab7a")
     AUTH_USERS = "7224758848"
