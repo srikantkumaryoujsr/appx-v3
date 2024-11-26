6ca8a79db80e6bf6bb878be763ba0248eefd2391
🔴Vsp हिन्दू🟠🕉️ <vidyannajaysinghpatel@gmail.com>
2024-11-26 20:27:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7508390450:AAEp39kEUvmtwnmHhgZ2EO6QscIhG9hrRPE")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7527973795:AAFP7w-l0cSqTxiItAx55TXEbjex_Xe-2i4")
     API_ID = int(os.environ.get("API_ID", "20821267"))
     API_HASH = os.environ.get("API_HASH", "8723cdf433be176300044547ee6bab7a")
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://aiitassam:SY06t3delyAShe71@cluster0.ugawa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
