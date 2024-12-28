09929d72482419868e8a60cd4e08cc49b2d4ac38
VspjiDev <VspjiOfficial@gmail.com>
2024-12-28 19:03:13 +0530

    Update vsp.py

--- plugins/modules/vsp.py
+++ plugins/modules/vsp.py
@@ -40,7 +40,7 @@ import os
 from plugins.modules.subscription import check_subscription
 
 # MongoDB Configuration
-MONGO_URI = "mongodb+srv://chutiya:fghytrhfhdfghh@cluster0.iomlk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://Rajahd41:Rajahd41@cluster0.omrv9cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["bot_database"]
 config_collection = db["batch_configs"]
